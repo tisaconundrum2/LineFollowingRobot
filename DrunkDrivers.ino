b556b865ddfbad8f272b69d1d4ada462424b3db4
Nicholas Finch <ngf4@nau.edu>
2017-12-03 08:05:09 -0700

    Update DrunkDrivers.ino

--- DrunkDrivers.ino
+++ DrunkDrivers.ino
@@ -34,7 +34,7 @@ void test() {
   for (int pos = 0; pos <= 200; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     setAction(pos, 0);              // tell servo to go to position in variable 'pos'
-    delay(100);                       // waits 15ms for the servo to reach the position
+    delay(100);                       // waits 100ms for the servo to reach the position
   }
 }
 
