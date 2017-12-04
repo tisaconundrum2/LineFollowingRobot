b9d2e827b0f90e389fdab08ead85efb5c6f23a32
Nicholas Finch <ngf4@nau.edu>
2017-12-04 09:28:14 -0700

    Fixing directories and editing code
    Organizing files and code.

--- DrunkDrivers/DrunkDrivers.ino
+++ DrunkDrivers/DrunkDrivers.ino
@@ -1,30 +1,44 @@
 #include <Servo.h>
 Servo leftServo;
 Servo rightServo;
-
+int isObstaclePin = 8;
+int isObstacle = HIGH;
 
 void setup() {
-  leftServo.attach(9); //Pin 9 for Servo Output
-  rightServo.attach(10); // Pin 10 for Servo Output
+  leftServo.attach(9);
+  rightServo.attach(10);
+  pinMode(isObstaclePin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  runn();
+//  test();
 }
 
-void leftFoward(){
-  leftServo.write(135);
+void runn() {
+  isObstacle = digitalRead(isObstaclePin);
+  if (isObstacle == LOW) {
+    delay(20); // read yellow paper
+    setAction(130, 0); // slight backward, forward
+  } else {
+    delay(20); // read black tape
+    setAction(200, 165); // forward, slight backward
+  }
 }
 
-void leftbackward(){
-  leftServo.write(45);  
+void setAction(int leftSpeed, int rightSpeed) {
+  leftServo.write(leftSpeed);
+  rightServo.write(rightSpeed);
+  Serial.print(leftSpeed);
+  Serial.print(rightSpeed);
+  Serial.println();
 }
 
-void rightFoward(){
-  rightServo.write(135);
+void test() {
+  for (int pos = 0; pos <= 200; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    setAction(pos, 0);              // tell servo to go to position in variable 'pos'
+    delay(100);                       // waits 100ms for the servo to reach the position
+  }
 }
-
-void rightBackward(){
-  rightServo.write(45);
-}
-
